663b1974cb5b55472f4b1be8a88fe2fc2980f33f
wesleyalves <110191958+Wesley1980alves@users.noreply.github.com>
2022-09-13 23:06:51 -0300

    Create MoudarVariavel.java
    estudando o casting

---
+++ MoudarVariavel.java
@@ -0,0 +1,20 @@
+package aulavariavel2;
+
+public class MoudarVariavel {
+
+	public static void main(String[] args) {
+		double d=5;
+		float f = 3;
+		int g =20;
+		float x = f +  (float) d;
+		float h = g + (float) f;
+		float p = f + (int ) g;
+		System.out.println("valor de x :"+ x);
+		System.out.println("valor de h :"+ h);
+		System.out.println("valor de p :"+p);
+		
+		//este programinha simples para  entender como funciona o casting// 
+		
+	}
+
+}
